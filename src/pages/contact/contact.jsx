2d2e56ab6584f9b5bdadcd5c056e9b2d970a98e9
horspaz064 <hormc1234@gmial.com>
2025-07-21 14:41:41 +0700

    commit change

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -69,7 +69,7 @@ const Contact = () => {
                 <button
                   style={{ float: 'right'}}
                   className={styles.btnSubmit}>
-                  Submit
+                  Send
                 </button>
               </div>
             </form>
